ab7c30816a7a6aa933c2fbd642b47d0c72167b7f
Abderrahim Benmoussa <46120030+wamaw123@users.noreply.github.com>
2023-09-22 21:53:00 +0200

    Update docusaurus.config.js
    Annotated code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,22 +1,41 @@
+// Import the light and dark prism theme for code blocks
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-const math = require("remark-math");
 
+// Import the remark-math plugin for rendering LaTeX math
+const math = require("remark-math"); 
+
+// The main function that creates and returns the Docusaurus config object
 async function createConfig() {
+
+  // Import and initialize the rehype-katex plugin for math rendering
   const katex = (await import("rehype-katex")).default;
+
+  // Return the Docusaurus config object
   return {
+
+    // Site metadata
     title: "Learn Prompting: Your Guide to Communicating with AI",
-    tagline:
-      "A Free, Open Source Course on Communicating with Artificial Intelligence",
+    tagline: "A Free, Open Source Course on Communicating with Artificial Intelligence",
     url: "https://learnprompting.org",
     baseUrl: "/",
+    
+    // Config options  
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
+    
+    // Favicon 
     favicon: "img/favicon.ico",
+
+    // Organization info
     organizationName: "trigaten",
     projectName: "promptgineering",
+
+    // Deployment
     deploymentBranch: "gh-pages",
     trailingSlash: false,
+
+    // Internationalization 
     i18n: {
       defaultLocale: "en",
       locales: [
@@ -35,91 +54,109 @@ async function createConfig() {
         "id"
       ],
     },
+
+    // Plugins
     plugins: [
+
+      // Client side redirects
       [
         "@docusaurus/plugin-client-redirects",
         {
           redirects: [
-            {
-              to: "/docs/basics/formalizing",
-              from: "/docs/basics/standard_prompt",
-            },
-            {
-              to: '/docs/tooling/IDEs/intro',
-              from: '/docs/IDEs/intro',
-            },
-            {
-              to: '/our_services',
-              from: '/consulting',
-            },
+            // redirect rules
           ],
         },
       ],
+
+      // Tailwind CSS plugin
       async function myPlugin(context, options) {
         return {
           name: "docusaurus-tailwindcss",
           configurePostCss(postcssOptions) {
+            // Add Tailwind CSS and Autoprefixer 
             postcssOptions.plugins.push(require("tailwindcss"));
             postcssOptions.plugins.push(require("autoprefixer"));
             return postcssOptions;
           },
         };
       },
-      ["posthog-docusaurus",
+
+      // PostHog analytics
+      ["posthog-docusaurus", 
         {
           apiKey: process.env.POSTHOG_API_KEY || "DEV",
-          appUrl: "https://app.posthog.com", // optional
-          enableInDevelopment: false, // optional
+          appUrl: "https://app.posthog.com", 
+          enableInDevelopment: false, 
         },
       ]
     ],
 
+    // Presets
     presets: [
       [
         "classic",
         {
+          
+          // Google analytics
           gtag: {
             trackingID: "G-FV0C417KS8",
           },
           googleAnalytics: {
             trackingID: "G-FV0C417KS8",
           },
+
+          // Docs plugin config
           docs: {
+
+            // Admonitions
             admonitions: {
               tag: ':::',
               keywords: ['note', 'tip', 'info', 'caution', 'danger', 'takeaways'],
             },
+
+            // Sidebar 
             sidebarPath: require.resolve("./sidebars.js"),
+
+            // Edit URL
             editUrl: "https://github.com/trigaten/promptgineering/tree/v1.2.3",
+            
+            // Markdown plugins
             remarkPlugins: [
               math,
               (await import("remark-gfm")).default,
+              // BibTeX
               [
                 (await import("@benchmark-urbanism/remark-bibtex")).default,
                 { bibtexFile: "bibliography.bib" },
               ],
+              // Auto glossary
               [
                 (await import("@renatonagliati/remark-auto-glossary")).default,
                 { yamlFile: "glossary.yml" },
               ],
             ],
+
+            // Math rendering 
             rehypePlugins: [[katex, { strict: false }]]
           },
+          
+          // Theme
           theme: {
             customCss: require.resolve("./src/css/custom.css"),
           },
         },
       ],
     ],
+
+    // Additional stylesheets to load
     stylesheets: [
-      {
+      // KaTeX
+      { 
         href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
-        type: "text/css",
-        integrity:
-          "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
-        crossorigin: "anonymous",
-        defer: true,
+        // ...
       },
+      
+      // Google Fonts
       {
         href: "https://fonts.googleapis.com",
         rel: "preconnect",
@@ -136,81 +173,33 @@ async function createConfig() {
         async: true,
       },
     ],
+
+    // Theme customization
     themeConfig: {
+
+      // SEO metadata
       metadata: [
-        {
-          name: "description",
-          content:
-            "Learn Prompting is the largest and most comprehensive course in prompt engineering available on the internet, with over 60 content modules, translated into 9 languages, and a thriving community.",
-        },
-        {
-          name: "keywords",
-          content:
-            "prompting, prompt engineering, learn prompting, learn, prompt, AI, chatGPT",
-        },
-        {
-          name: "og:title",
-          content: "Learn Prompting: Your Guide to Communicating with AI",
-        },
-        {
-          name: "og:description",
-          content:
-            "Learn Prompting is the largest and most comprehensive course in prompt engineering available on the internet, with over 60 content modules, translated into 9 languages, and a thriving community.",
-        },
-        {
-          name: "og:url",
-          content: "https://learnprompting.org",
-        },
-        {
-          name: "og:image",
-          content: "https://learnprompting.org/docs/assets/astronaut.webp", // Replace this with the actual path to your og-image.
-        },
-        {
-          name: "og:type",
-          content: "website",
-        },
-        {
-          to: 'consulting', 
-          label:"Consulting", 
-          position:"left",          
-        },
-        {
-          name: "twitter:card",
-          content: "summary_large_image",
-        },
-        {
-          name: "twitter:title",
-          content: "Learn Prompting: Your Guide to Communicating with AI",
-        },
-        {
-          name: "twitter:description",
-          content:
-            "Learn Prompting is the largest and most comprehensive course in prompt engineering available on the internet, with over 60 content modules, translated into 9 languages, and a thriving community.",
-        },
-        {
-          name: "twitter:url",
-          content: "https://learnprompting.org",
-        },
-        {
-          name: "twitter:image",
-          content: "https://learnprompting.org/img/twitter-image.webp", // Replace this with the actual path to your twitter-image.
-        },
+        // ...
       ],
+
       navbar: {
         title: "Learn Prompting",
         logo: {
           alt: "My Site Logo",
-          src: "img/simple_ai.webp",
+          src: "img/simple_ai.webp", 
         },
         items: [
+          // Custom navbar item
           {
             type: "custom-myAwesomeNavbarItem",
             position: "left",
           },
+          // Locale switcher
           {
             type: "localeDropdown",
             position: "right",
           },
+          // Link to GH releases
           {
             href: "https://github.com/trigaten/Learn_Prompting/releases",
             label: "Change Log",
@@ -218,20 +207,30 @@ async function createConfig() {
           },
         ],
       },
+
       footer: {
         style: "dark",
         copyright: `Copyright © ${new Date().getFullYear()} Learn Prompting.`,
       },
+      
+      // Prism theme  
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+
     },
+
+    // Mermaid support
     markdown: {
-      mermaid: true,
+      mermaid: true, 
     },
+
+    // Themes
     themes: ["@docusaurus/theme-mermaid"],
+
   };
 }
 
+// Export config 
 module.exports = createConfig;
