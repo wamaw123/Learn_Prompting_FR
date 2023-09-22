92d7cc446c4f58d868b1f641a6a343b04a48a6ca
Abderrahim Benmoussa <46120030+wamaw123@users.noreply.github.com>
2023-09-22 21:55:01 +0200

    Update babel.config.js
    code commented

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
+// Exporting the module configuration
 module.exports = {
+  // Using the Docusaurus Babel preset for transpilation
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+  // Enabling the "compact" option to minimize the output code
   compact: true,
 };
